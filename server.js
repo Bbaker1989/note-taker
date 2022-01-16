2474332889eaf1f01e6d43f7060c9694a2daf63e
Bbaker1989 <86622638+Bbaker1989@users.noreply.github.com>
2022-01-16 14:40:14 -0500

    updated notes

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const express = require('express');
 const app = express ();
 const PORT = process.env.PORT || 3001;
 
-//require API and HTML routes
-
+//require HTML routes
 const htmlRoutes = require('./routes/htmlRoutes');
 
 //app.use to parse incoming data
@@ -14,8 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//app.use router to set up different API routes
-
+//app.use router to set up different html routes
 app.use('/', htmlRoutes);
 
 
